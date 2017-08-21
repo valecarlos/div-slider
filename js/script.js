86596dac61b09b9b47f1b356ccd2acd70b4b1e1e
Carlos <carlos_valencia@live.com>
2017-08-21 16:34:13 -0500

    code refactor for revealing module pattern

--- js/script.js
+++ js/script.js
@@ -1,56 +1,71 @@
 $(document).ready(function(){
-	var SliderModule = (function(){
-		//create a new empty object which will handle the slider
-		var pb = {};
-		pb.el = $('#slider');
-		pb.items = pb.el.find("[class^='main-header']");
-
+	var Slider = function(options){
+		//recieve options, we need the -el- attribute and the duration of the slide on screen
+		var slider = {};
 		var currentSlide = 0;
 		var nextSlide = 1;
-		var sliderLength = pb.items.length;
-		//offset calculates from which position do we have to start to link the index from the slide and the slider button
-		var offset = pb.el.children().length - sliderLength;
-		//initialize
-		pb.init = function(settings){
-			//Activate the slider
-			SliderInit();
-		}
+		var sliderInterval = false;
 
-		var SliderInit = function(){
-			pb.createNavigation();
-			setSliderInterval();
-		};
+		slider.el = $(options.el);
+		slider.sliderTimeInterval = options.sliderTimeInterval;
+		slider.items = slider.el.find("[class^='main-header']");
+		slider.sliderLength = slider.items.length;
+		slider.offset = slider.el.children().length - slider.sliderLength;
+		_createNavigation();
+		startSliderInterval();
 
-		var setSliderInterval = function(){
-			sliderInterval = setInterval(pb.startSlider, 4000);
-		};
-
-		pb.createNavigation = function(){
-			var ulSlider = pb.el.find('.slider-nav');
+		function _createNavigation(){
+			//create the -buttons- to navigate between slides
+			var ulSlider = slider.el.find('.slider-nav'); //position this where you want your navigation buttons to be
 			var newLI = '<li></li>';
 			var newLIActive = '<li class= "active-button"></li>';
-			for (var i = 0; i< sliderLength ; i++){
+			for (var i = 0; i< slider.sliderLength ; i++){
 				if (i === 0){
 					ulSlider.append(newLIActive);	
 				}
 				else{
 					ulSlider.append(newLI);
 				}
-				
+			}
+			_addEventListeners();
+		};
+
+		function _addEventListeners(){
+			// add event binders to the dynamically added li elements
+			slider.el.find('.slider-nav').on('click', 'li', function() {
+				//set this -> this will be the  to be the next slide, stop the function interval and start it again 
+				nextSlide = $(this).index();
+				stopSliderInterval();
+				_startSlider();
+				startSliderInterval();
+			});
+		}
+
+		function startSliderInterval (){
+			if (!sliderInterval){
+				sliderInterval = setInterval(_startSlider, slider.sliderTimeInterval);
 			}
 		};
 
-		pb.startSlider = function(){
-			var mainDivs = pb.items;
-			var ulSlider = pb.el.find('.slider-nav');
-			if(nextSlide >= sliderLength){
+		function stopSliderInterval(){
+			if (sliderInterval){
+				clearInterval(sliderInterval);
+				sliderInterval = false; //to easily know the interval is not running as per https://stackoverflow.com/questions/2679141/is-there-a-way-to-check-if-a-var-is-using-setinterval
+			}
+		}
+
+		function _startSlider(){
+			var mainDivs = slider.items;
+			var ulSlider = slider.el.find('.slider-nav');
+			//which slide comes next?
+			if(nextSlide >= slider.sliderLength){
 				nextSlide = 0;
-				currentSlide = sliderLength -1;
+				currentSlide = slider.sliderLength -1;
 			}
 
 			//animations using the eq selector
 			//we first add the animation class, and then remove the previous one we don't want
-			//toggle class results in an unwanted behaviour
+			//toggle class, it results in an unwanted behaviour
 			mainDivs.eq(currentSlide).find('h1').addClass('fade-out-bottom-top');
 			mainDivs.eq(currentSlide).find('h1').removeClass('fade-in-top-bottom');
 
@@ -75,7 +90,7 @@ $(document).ready(function(){
 
 			//find offset of child elements to use their index to match the current slide with the selected button
 			ulSlider.children().removeClass("active-button");
-			ulSlider.children().eq(mainDivs.eq(nextSlide).index() - offset).addClass("active-button");
+			ulSlider.children().eq(mainDivs.eq(nextSlide).index() - slider.offset).addClass("active-button");
 
 
 			//update variables
@@ -83,21 +98,22 @@ $(document).ready(function(){
 			nextSlide += 1;
 		};
 
+		//return the function we want to reveal
+		return {
+			startSliderInterval: startSliderInterval,
+			stopSliderInterval: stopSliderInterval
+		}
 		
-		//add event binders to the dynamically added li elements
-		$('.slider-nav').on('click', 'li', function() {
-			//set this to be the next slide, stop the function interval and start it again 
-			nextSlide = $(this).index();
-			clearInterval(sliderInterval);
-			pb.startSlider();
-			setSliderInterval();
-			
-		});
+	}
 
-		return pb;
+	var headerSlider = new Slider({el: '#slider', sliderTimeInterval: 4000});
 
-	}()); //autoexecute on ready
+	//just to try the functions provided by the module work
+	$("#slider a").on('mouseover', function(){
+		headerSlider.stopSliderInterval();
+	});
 
-	SliderModule.init();
-
+	$("#slider a").on('mouseout', function(){
+		headerSlider.startSliderInterval();
+	});
 });
